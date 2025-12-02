5355dde5c17505f04b87727e1f3f1991d982f97f
liia <kirilich_lubit@rambler.ru>
2025-12-02 11:23:39 +0300

    fix canvas realise flush entirely

--- main.cpp
+++ main.cpp
@@ -60,10 +60,19 @@ int main() {
 	return err;
 }
 
+void topit::flush(std::ostream& os, const char* cnv, f_t fr) {
+	for (size_t i = 0; i < rows(fr); i++) {
+		for (size_t j = 0; j < cols(fr); ++j) {
+			os << cnv[i * cols(fr) + j];
+		}
+		os << "\n";
+	}
+}
+
 char* topit::canvas(f_t fr, char fill) {
 	size_t s = rows(fr) * cols(fr);
 	char* c = new char[s];
-	for (size_t i = 0; i < c; ++i) {
+	for (size_t i = 0; i < s; ++i) {
 		c[i] = fill;
 	}
 	return c;

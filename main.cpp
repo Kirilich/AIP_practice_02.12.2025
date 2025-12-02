6b08ba4ab19b73590ac9aeff1498531fd84a9222
liia <kirilich_lubit@rambler.ru>
2025-12-02 11:50:31 +0300

    realise append entirely realise extend for append error

--- main.cpp
+++ main.cpp
@@ -23,6 +23,8 @@ namespace topit {
 		p_t next(p_t prev) const override;
 		p_t d;
 	};
+	p_t* extend(const p_t* pts, size_t s, p_t fill);
+	void extend(p_t** pts, size_t& s, p_t fill);
 	void append(const IDraw* sh, p_t** ppts, size_t& s);
 	f_t frame(const p_t* pts, size_t s);
 	char* canvas(f_t fr, char fill);
@@ -60,9 +62,35 @@ int main() {
 	return err;
 }
 
+topit::p_t* topit::extend(const p_t* pts, size_t s, p_t fill) {
+	p_t* r = new p_t[s + 1];
+	for (size_t i = 0; i < s; ++i) {
+		r[i] = pts[i];
+	}
+	r[s] = fill;
+	return r;
+}
+
+void topit::extend(p_t** pts, size_t& s, p_t fill) {
+	p_t* r = extend(*pts, s, fill);
+	delete[] * pts;
+	++s;
+	*pts = r;
+}
+
+
+void topit::append(const IDraw* sh, p_t** ppts, size_t& s) {
+	extend(ppts, s, sh->begin());//закнуть начало в массив
+	p_t b = sh->begin();
+	while (sh->next(b) != sh->begin()) {
+		b = sh->next(b);
+		extend(ppts, s, b);
+	}
+}
+
 void topit::paint(p_t p, char* cnv, f_t fr, char fill) {
 	size_t dx = p.x - fr.aa.x;
-	sixe_t dy = fr.bb.y - p.y;
+	size_t dy = fr.bb.y - p.y;
 	cnv[dy * cols(fr) + dx] = fill;
 }
 

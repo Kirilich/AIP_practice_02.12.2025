6be1dc9e7e08ff3b41c95214145074a4492b06a7
liia <kirilich_lubit@rambler.ru>
2025-12-02 11:11:32 +0300

    realise append realise frame entirely realise canvas realise paint realise flush

--- main.cpp
+++ main.cpp
@@ -21,23 +21,55 @@ namespace topit {
 		p_t next(p_t prev) const override;
 		p_t d;
 	};
+	void append(const IDraw* sh, p_t** ppts, size_t& s);
+	f_t frame(const p_t* pts, size_t s);
+	char* canvas(f_t fr, char fill);
+	void paint(p_t p, char* cnv, f_t fr, char fill);
+	void flush(std::ostream& os, const char* cnv, f_t fr);
 }
 
 int main() {
 	using namespace topit;
 	int err = 0;
-	IDraw* shp[3] = {};
+	IDraw* shp[3] = {}; 
+	p_t* pts = nullptr;
+	size_t s = 0;
 	try {
 		shp[0] = new Dot({ 0, 0 });
 		shp[1] = new Dot({ 2, 3 });
+		shp[2] = new Dot({ -5, -2 });
+		for (size_t i = 0; i < 3; ++i) {
+			append(shp[i], &pts, s);
+		}
+		f_t fr = frame(pts, s);
+		char* cnv = canvas(fr, '.');
+		for (size_t i; i < s; ++i) {
+			paint(pts[i], cnv, fr, '7');
+		}
+		flush(std::cout, cnv, fr);
+		delete [] cnv;
 	} catch(...) {
 		std::cerr << "Error!\n";
 		err = 1;
 	}
-
+	delete shp[2];
 	delete shp[1];
 	delete shp[0];
 	return err;
+}
+
+topit::f_t topit::frame(const p_t* pts, size_t s) {
+	int minx = pts[0].x, miny = pts[0].y;
+	int maxx = minx, maxy = miny;
+	for (size_t i = 0; i < s; ++i) {
+		minx = std::min(minx, pts[i].x); //pts[i].x < minx ? pts[i].x : minx;
+		miny = std::min(miny, pts[i].y);
+		maxx = std::max(maxx, pts[i].x);
+		maxy = std::max(maxy, pts[i].y);
+	}
+	p_t a{ minx, miny };
+	p_t b{ maxx, maxy };
+	return f_t{ a, b };
 }
 
 topit::Dot::Dot(p_t dd):

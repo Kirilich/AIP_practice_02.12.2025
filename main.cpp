0d813967d9115e54291cddf7649e5dfaf510c8bc
liia <kirilich_lubit@rambler.ru>
2026-01-20 22:34:51 +0300

    fix main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,24 @@
 int main() {
 	using namespace topit;
 	int err = 0;
-	IDraw* shp[11] = {}; 
+	IDraw* shp[12] = {}; 
 	p_t* pts = nullptr;
 	size_t s = 0;
 	try {
-		shp[0] = new Diamond({15, 10}, 5);
-		for (size_t i = 0; i < 1; ++i) {
+		shp[0] = new HLine({ -3, 2 }, 7);
+		shp[1] = new VLine({ -4, -2 }, 5);
+		shp[2] = new VLine({ 4, -2 }, 5);
+		shp[3] = new HLine({ -3, -2 }, 7);
+		shp[4] = new Dot({ -3, 3 });
+		shp[5] = new Dot({ -2, 4 });
+		shp[6] = new Dot({ -1, 5 });
+		shp[7] = new Dot({ 0, 6 });
+		shp[8] = new Dot({ 1, 5 });
+		shp[9] = new Dot({ 2, 4 });
+		shp[10] = new Dot({ 3, 3 });
+		shp[11] = new Diamond({ 15, 10 }, 5);
+
+		for (size_t i = 0; i < 12; ++i) {
 			append(shp[i], &pts, s);
 		}
 		f_t fr = frame(pts, s);
@@ -24,9 +36,8 @@ int main() {
 		std::cerr << "Error!\n";
 		err = 1;
 	}
-	delete shp[3];
-	delete shp[2];
-	delete shp[1];
-	delete shp[0];
+	for (size_t i = 0; i < 12; i++) {
+		delete shp[i];
+	}
 	return err;
 }
